1da735648393d39bedfeb39ab375f3fc0c7e502d
Estefany Gomes <estefany.gomes@ba.estudante.senai.br>
2025-11-12 16:41:48 -0300

    update 2

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable, tap, catchError, throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
